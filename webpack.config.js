6464f366e4803ec52c0b796e7e03f4646054c829
Kyaw Aung Win <kyawagwin@gmail.com>
2017-04-19 16:30:25 +0800

    initial setup

---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const webpack = require("webpack");
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+
+const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
+    template: "./client/index.html",
+    filename: "index.html",
+    inject: "body"
+});
+
+const ReactBuildConfig = new webpack.DefinePlugin({
+    'process.env': {
+    NODE_ENV: JSON.stringify('production')
+    }
+});
+
+const UglifyJsPluginsConfig = new webpack.optimize.UglifyJsPlugin({
+    compress: {
+        warnings: false
+    }
+});
+
+module.exports = {
+    entry: "./client/index.js",
+    output: {
+        path: path.resolve("dist"),
+        filename: "index_bundle.js"
+    },
+    module: {
+        loaders: [
+            { test: /\.js$/, loader: "babel-loader", exclude: /node_modules/},
+            { test: /\.jsx$/, loader: "babel-loader", exclude: /node_modules/}
+        ]
+    },
+    stats: {
+        colors: true
+    },
+    devtool: 'source-map',
+    plugins: [
+        //ReactBuildConfig,
+        //UglifyJsPluginsConfig,
+        HtmlWebpackPluginConfig
+    ]
+}
