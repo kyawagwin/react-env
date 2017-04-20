3fe088e5bd21593345eba7de024670f01372819a
Kyaw Aung Win <kyawagwin@gmail.com>
2017-04-20 12:03:08 +0800

    eslint
    eslint with -> eslint-plugin-react

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -3,9 +3,7 @@ import React from "react";
 export default class App extends React.Component {
     render() {
         return (
-            <div style={{ textAlign: "center" }}>
-                <h1>Hello World!</h1>
-            </div>
+            <h3 className="ui center aligned header">Hello World!</h3>
         );
     }
 }
